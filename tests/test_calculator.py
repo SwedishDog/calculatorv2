9f18b75fdb204a1b18ad078aaca6117f953516fb
SwedishDog <100093667+SwedishDog@users.noreply.github.com>
2022-02-21 12:31:31 -0600

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,6 +8,8 @@ class TestCalc(unittest.TestCase):
         Number1 = random.randint(0,1000000)
         Number2 = random.randint(0,1000000)
 
+        ExpectedValue = Number1 + Number2
+
         result = ConversionRate(Number1, Number2)
 
-        self.assertEqual(result, 10)
+        self.assertEqual(result, ExpectedValue)
